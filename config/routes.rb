1250b4adb0a85ebeccbd6701ea8f7158a5dfc854
aster68699 <yfd68699@nifty.com>
2021-05-05 00:47:25 +0900

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
-  get '/books/index' => "books#index", as: "books_index"
+  root "books#index", as: "books_index"
   post "/books/create" => "books#create"
   get "/books/new" => "books#new", as: "books_new"
   get "/books/:id/edit" => "books#edit", as: "books_edit"
   get "/books/:id" => "books#show", as: "book"
   patch "/books/:id" => "books#update"
   delete "/books/:id" => "books#destroy"
-  root "home#top"
   
 end
